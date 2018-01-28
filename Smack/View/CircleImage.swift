f9455563572331eaa77fe3dae145b64c7f99f1e5
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-01-28 11:13:24 +0530

    adding round background

---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by SIMRANPREET KAUR on 28/01/18.
+//  Copyright © 2018 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+    override func awakeFromNib() {
+        setupView()
+    }
+    func setupView(){
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+}
