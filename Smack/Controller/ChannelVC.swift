80c95e832c3d9b6e6817596575ff82b39f142ca1
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-01-28 16:18:47 +0530

    adding observer

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -12,13 +12,28 @@ class ChannelVC: UIViewController {
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){
         
     }
-
+    @IBOutlet weak var loginbtn: UIButton!
+    
+    @IBOutlet weak var profimg: CircleImage!
+    @IBOutlet weak var loginnext: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_notif:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
 
     @IBAction func loginbtnPressed(_ sender: Any) {
         performSegue(withIdentifier:"tologin", sender: nil)
+    }
+  @objc  func userDataDidChange(_notif: Notification){
+    if AuthService.instance.isLoggedIn{
+        loginnext.setTitle(UserDataService.instance.name, for: .normal)
+        profimg.image=UIImage(named: UserDataService.instance.avatarName)
+    }else{
+        loginnext.setTitle("Login", for: .normal)
+        profimg.image=UIImage(named: "menuProfileIcon")
+        profimg.backgroundColor = UIColor.clear
+    }
+        
     }
 }
